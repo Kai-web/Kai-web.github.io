18719d7299bd906e028d630d7a246660b3783907
Kai-web <136154931@qq.com>
2022-10-15 12:25:13 +0800

    顶部加载条

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,8 @@
+import DefaultTheme from 'vitepress/theme'
 import vitepressNprogress from 'vitepress-plugin-nprogress'
 
 export default {
+  ... DefaultTheme,
   enhanceApp: (ctx) => {
     vitepressNprogress(ctx)
   }
